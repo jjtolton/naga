e30cbdb6f4c93d080ebbd1f4ec34b1a5667c60db
Jay <jatolton@cisco.com>
2016-07-30 05:41:55 -0400

    added MANIFEST, housekeeping on setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ setup(
   name = 'naga',
   packages = ['naga'], # this must be the same as the name above
   version = '0.1',
-  description = 'A powerful Python/Lisp-hybrid-monster',
+  description = 'A powerful Python/Lisp-hybrid-monster!',
   author = 'James J. Tolton',
   author_email = 'jjtolton@gmail.com',
-  url = 'https://github.com/hiimjayhireme/naga', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  url = 'https://github.com/hiimjayhireme/naga',
+  download_url = 'https://github.com/hiimjayhireme/naga/tarball/0.1',
+  keywords = ['testing', 'logging', 'example'],
   classifiers = [],
 )
