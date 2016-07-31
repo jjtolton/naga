92df62e94ffc0aedffaa4a79731cd5fd1e4b3c34
Jay <jatolton@cisco.com>
2016-07-31 00:19:23 -0400

    bumped version #

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ if __name__ == '__main__':
     setup(
         name='naga',
         packages=['naga'],  # this must be the same as the name above
-        version='0.1.02',
+        version='0.1.03',
         description='A powerful Python/Lisp-hybrid-monster!',
         author='James J. Tolton',
         author_email='jjtolton@gmail.com',
         url='https://github.com/hiimjayhireme/naga',
-        download_url='https://github.com/hiimjayhireme/naga/tarball/0.1',
         keywords=['testing', 'logging', 'example'],
         classifiers=[],
     )
