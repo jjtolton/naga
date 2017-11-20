e0ed457c56f65a89e8979277c453e02a34792c73
J.J. Tolton <jjtolton@gmail.com>
2017-11-20 11:41:13 -0500

    bump version #

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='naga',
     packages=['naga'],  # this must be the same as the name above
-    version='0.3.2.3',
+    version='0.3.2.4',
     description='A powerful Clojure-inspired Python/Lisp-hybrid-monster!',
     author='James J. Tolton',
     author_email='jjtolton@gmail.com',
